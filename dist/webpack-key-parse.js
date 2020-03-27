facb552636c567badb4e4514e3c8ed74199742d3
李灵菲 <593029372@qq.com>
2020-03-27 09:20:21 +0800

    fix: support no relative path

--- dist/webpack-key-parse.js
+++ dist/webpack-key-parse.js
@@ -1,5 +1,5 @@
 /**
- * webpack-key-parse v0.0.1 
+ * webpack-key-parse v0.0.3 
  */
-!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?t(exports):"function"==typeof define&&define.amd?define(["exports"],t):t((e=e||self).WebpackKeyParse={})}(this,(function(e){"use strict";e.parse=e=>{const t=(e=e.slice(2)).lastIndexOf("."),s=e.lastIndexOf("/");let n="",i="",r="",d="",c="";return n=e.slice(t),s>-1&&(i=e.slice(0,s)),{name:r=e.slice(s+1,t),ext:n,base:d=r+n,dir:i,entry:c=i?i+"/"+r:r,dir_dash:i.replace(/\//g,"-"),entry_dash:c.replace(/\//g,"-")}},Object.defineProperty(e,"__esModule",{value:!0})}));
+!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?t(exports):"function"==typeof define&&define.amd?define(["exports"],t):t((e=e||self).WebpackKeyParse={})}(this,(function(e){"use strict";e.parse=e=>{0===e.indexOf("./")&&(e=e.slice(2));const t=e.lastIndexOf("."),n=e.lastIndexOf("/");let s="",i="",d="",r="",f="";return s=e.slice(t),n>-1&&(i=e.slice(0,n)),{name:d=e.slice(n+1,t),ext:s,base:r=d+s,dir:i,entry:f=i?i+"/"+d:d,dir_dash:i.replace(/\//g,"-"),entry_dash:f.replace(/\//g,"-")}},Object.defineProperty(e,"__esModule",{value:!0})}));
 //# sourceMappingURL=webpack-key-parse.js.map
--- src/webpack-key-parse.js
+++ src/webpack-key-parse.js
@@ -1,26 +1,28 @@
 export const parse = keyPath => {
-  keyPath = keyPath.slice(2)
-  const lastDotIndex = keyPath.lastIndexOf('.')
-  const lastSlashIndex = keyPath.lastIndexOf('/')
-  let ext = ''
-  let dir = ''
-  let name = ''
-  let base = ''
-  let entry = ''
-  ext = keyPath.slice(lastDotIndex)
-  if (lastSlashIndex > -1) {
-    dir = keyPath.slice(0, lastSlashIndex)
+  if (keyPath.indexOf("./") === 0) {
+    keyPath = keyPath.slice(2);
   }
-  name = keyPath.slice(lastSlashIndex + 1, lastDotIndex)
-  base = name + ext
-  entry = dir ? dir + '/' + name : name
+  const lastDotIndex = keyPath.lastIndexOf(".");
+  const lastSlashIndex = keyPath.lastIndexOf("/");
+  let ext = "";
+  let dir = "";
+  let name = "";
+  let base = "";
+  let entry = "";
+  ext = keyPath.slice(lastDotIndex);
+  if (lastSlashIndex > -1) {
+    dir = keyPath.slice(0, lastSlashIndex);
+  }
+  name = keyPath.slice(lastSlashIndex + 1, lastDotIndex);
+  base = name + ext;
+  entry = dir ? dir + "/" + name : name;
   return {
     name,
     ext,
     base,
     dir,
     entry,
-    dir_dash: dir.replace(/\//g, '-'),
-    entry_dash: entry.replace(/\//g, '-')
-  }
-}
+    dir_dash: dir.replace(/\//g, "-"),
+    entry_dash: entry.replace(/\//g, "-")
+  };
+};
